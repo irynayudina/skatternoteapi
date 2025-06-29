ea511e368e045ccdb5500303bbc26ffc74833a6e
Iryna Yudina <iryna.yudina@chisw.com>
2025-06-29 19:00:50 +0300

    origin

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   
   // Enable CORS for frontend
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://localhost:3000', 'https://scatternote.netlify.app'], // Frontend URLs
+    origin: ['http://localhost:5173', 'https://skatternoteapi-production.up.railway.app/',  'http://localhost:3000', 'https://scatternote.netlify.app'], // Frontend URLs
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
